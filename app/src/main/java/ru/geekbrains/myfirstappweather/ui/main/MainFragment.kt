0bb3878fa8d28ca861907c0ac4d45fabc511a0fc
turkovaliudmila <ljudmila327@rambler.ru>
2021-05-23 14:43:45 +0300

    Задан внешний макета фрагмента, хард код помещен в ресурсы, к классу фрагмента подключен view binding

--- app/src/main/java/ru/geekbrains/myfirstappweather/ui/main/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/myfirstappweather/ui/main/MainFragment.kt
@@ -7,8 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.geekbrains.myfirstappweather.R
+import ru.geekbrains.myfirstappweather.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -18,13 +22,18 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
